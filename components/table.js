295bdcf3ede5b5d51a5909160d100b8f66dc68f3
Ali <alim.9171@gmail.com>
2022-04-26 05:49:24 +0500

    added

--- components/table.js
+++ components/table.js
@@ -4,35 +4,30 @@ import SearchIcon from "@mui/icons-material/Search";
 import upArrow from "../images/Up0.png";
 import downArrow from "../images/Down0.png";
 function Table(props) {
-	var frequency = 3;
+	var frequency = 5;
 	var amountPercentage = 1;
 	const [data, setData] = useState(props.data);
-	const [style, setStyle] = useState(0);
+	const [secondCopyOfData, setSecondCopyOfData] = useState(props.data);
+	const [styleState, setStyle] = useState(1);
 	const [numberOfItems, setNumberOfItems] = useState(10);
 	var copyOfData = props.data;
 	var internationalNumberFormat = new Intl.NumberFormat("en-US");
 	function roundOff(number, percentage) {
-		return (
-			Math.round((number * (Math.random() * percentage)) / 100 / 100000) *
-			100000
-		);
+		var retNumber = (number * (Math.random() * (percentage - 0.1) + 0.1)) / 100;
+		return Math.round(retNumber / 100000) * 100000 >= 50001
+			? Math.round(retNumber / 100000) * 100000
+			: Math.ceil(retNumber / 100000) * 100000;
 	}
-	useEffect(() => {
-		setStyle(0);
-		setTimeout(() => setStyle(1), 1000);
 
+	useEffect(() => {
 		var newArr = data.map((el) => el);
 		const timer = setInterval(() => {
+			setStyle(1);
+			// console.log(secondCopyOfData);
+			setSecondCopyOfData(data);
+
 			for (let index = 0; index < amountPercentage; index++) {
 				var randomNumber = Math.floor(Math.random() * data.length);
-				console.log(
-					Math.round(
-						(copyOfData[randomNumber].lp *
-							(Math.random() * copyOfData[randomNumber].deltalp)) /
-							100 /
-							100000
-					) * 100000
-				);
 				newArr[randomNumber] = {
 					...newArr[randomNumber],
 					lp:
@@ -74,8 +69,15 @@ function Table(props) {
 	}, [data]);
 
 	useEffect(() => {
-		// setNumberOfItems(10)
-	}, [numberOfItems]);
+		// console.log("entered");
+		var timeout = setInterval(() => {
+			setStyle(0);
+			// console.log("run");
+		}, 3000);
+		return () => {
+			clearInterval(timeout);
+		};
+	}, [data]);
 
 	return (
 		<div className={styles.wrapper}>
@@ -141,58 +143,66 @@ function Table(props) {
 							</tr>
 						</thead>
 						<tbody>
-							{data.map((el, i) => (
-								<tr>
-									<td className={styles.left}>{el.name}</td>
-									<td
-										className={styles.mid}
-										style={{
-											backgroundColor:
-												parseFloat(copyOfData[i].lp) > el.lp
-													? "#eddeea"
-													: parseFloat(copyOfData[i].lp) < el.lp
-													? "#edf4f3"
-													: "#fff",
-											// opacity:
-											// 	parseFloat(copyOfData[i].lp) > el.lp
-											// 		? style.toString()
-											// 		: parseFloat(copyOfData[i].lp) < el.lp
-											// 		? style.toString()
-											// 		: "1",
-											transition: "opacity 4s",
-										}}
-									>
-										PKR {internationalNumberFormat.format(Math.ceil(el.lp))}
-									</td>
-									<td
-										className={styles.mid}
-										style={{
-											backgroundColor:
-												parseFloat(copyOfData[i].hp) > el.hp
-													? "#eddeea"
-													: parseFloat(copyOfData[i].hp) < el.hp
-													? "#edf4f3"
-													: "#fff",
-											// opacity:
-											// 	parseFloat(copyOfData[i].hp) > el.hp
-											// 		? style.toString()
-											// 		: parseFloat(copyOfData[i].hp) < el.hp
-											// 		? style.toString()
-											// 		: "1",
-											transition: "opacity 4s",
-										}}
-									>
-										PKR {internationalNumberFormat.format(Math.ceil(el.hp))}
-									</td>
-									<td className={styles.right}>
-										{el.trend === "up" ? (
-											<img style={{ width: "35px" }} src={upArrow.src} />
-										) : (
-											<img style={{ width: "35px" }} src={downArrow.src} />
-										)}
-									</td>
-								</tr>
-							))}
+							{data.map((el, i) => {
+								// timer();
+								return (
+									<tr>
+										<td className={styles.left}>{el.name}</td>
+										<td
+											className={styles.mid}
+											style={{
+												backgroundColor:
+													styleState === 1
+														? secondCopyOfData[i].lp > el.lp
+															? "#eddeea"
+															: secondCopyOfData[i].lp < el.lp
+															? "#edf4f3"
+															: "#fff"
+														: "#fff",
+												// opacity:
+												// 	parseFloat(copyOfData[i].lp) > el.lp
+												// 		? style.toString()
+												// 		: parseFloat(copyOfData[i].lp) < el.lp
+												// 		? style.toString()
+												// 		: "1",
+												transition: "background-color 1s",
+											}}
+										>
+											PKR {internationalNumberFormat.format(Math.ceil(el.lp))}
+										</td>
+										{/* {console.log(el.hp, secondCopyOfData[i].hp)} */}
+										<td
+											className={styles.mid}
+											style={{
+												backgroundColor:
+													styleState === 1
+														? secondCopyOfData[i].hp > el.hp
+															? "#eddeea"
+															: secondCopyOfData[i].hp < el.hp
+															? "#edf4f3"
+															: "#fff"
+														: "#fff",
+												// opacity:
+												// 	parseFloat(copyOfData[i].hp) > el.hp
+												// 		? style.toString()
+												// 		: parseFloat(copyOfData[i].hp) < el.hp
+												// 		? style.toString()
+												// 		: "1",
+												transition: "background-color 1s",
+											}}
+										>
+											PKR {internationalNumberFormat.format(Math.ceil(el.hp))}
+										</td>
+										<td className={styles.right}>
+											{el.trend === "up" ? (
+												<img style={{ width: "35px" }} src={upArrow.src} />
+											) : (
+												<img style={{ width: "35px" }} src={downArrow.src} />
+											)}
+										</td>
+									</tr>
+								);
+							})}
 						</tbody>
 					</table>
 				</div>
